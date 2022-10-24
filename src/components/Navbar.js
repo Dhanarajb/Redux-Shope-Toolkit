8d73114e6ffdb7f96f7bcc136a3cbd48c9533584
Dhanaraj <dhanarajbhaskar690@gmail.com>
2022-10-24 15:40:35 +0530

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import './Nav.css'
 
 const Navbar = () => {
   const items = useSelector((state) => state.cart)
@@ -8,8 +9,8 @@ const Navbar = () => {
     <div className='Headerr' style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', height:'55px',backgroundColor:"#C0BDBD",padding:"10px",borderRadius:"10px" }}>
       <span className='logo'><span className='name'>R</span>EDUX <span className='name'>S</span>TORE 👉</span>
       <div>
-        <Link className='navLink' to='/'>Home</Link>
-        <Link className='navLink' to='/cart'>Cart</Link>
+        <Link className='navLink' to='/'>click on ☞<span><strong>Home</strong></span></Link>
+        <Link className='navLink' to='/cart'><strong>Cart</strong></Link>
         <span className='cartCount'>
           Cart Items: {items.length}
         </span>
